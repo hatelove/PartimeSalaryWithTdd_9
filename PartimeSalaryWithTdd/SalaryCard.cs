e366fb2da7593d682473c099264157cad15af79f
hatelove <jay.hatelove@gmail.com>
2017-09-02 13:23:57 +0800

    pass first test case

---
+++ PartimeSalaryWithTdd/SalaryCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PartimeSalaryWithTdd.Utility;
+
+namespace PartimeSalaryWithTdd
+{
+    public class SalaryCard
+    {
+        public SalaryCard()
+        {
+        }
+
+        public int HourlySalary { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public double CalculateSalary()
+        {
+            double workingHour = GetWorkingHour();
+            return workingHour * this.HourlySalary;
+        }
+
+        private double GetWorkingHour()
+        {
+            var morningHour = DateTimeHelper.TotalHours(this.StartTime,
+                new DateTime(this.StartTime.Year, this.StartTime.Month, this.StartTime.Day, 12, 0, 0));
+
+            var afternoonHour = DateTimeHelper.TotalHours(
+                new DateTime(this.EndTime.Year, this.EndTime.Month, this.EndTime.Day, 13, 0, 0), this.EndTime);
+
+            return morningHour + afternoonHour;
+        }
+    }
+}
